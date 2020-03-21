62baabe14dc89a5db5be73934e6aa4e5022d6883
sarori <salo3678@gmail.com>
2020-03-21 22:48:29 +0100

    .

---
+++ src/routes/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+`;
+
+// const Header = styled;
+
+export default () => {
+	return <Container></Container>;
+};
